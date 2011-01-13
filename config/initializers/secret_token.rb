56d4b8ed7234506a64676556f78a13396e0ea875
Charles Max Wood <chuck@teachmetocode.com>
2011-01-13 10:49:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freemark::Application.config.secret_token = '5b90b310fcea51f4254dc2fdb3c36d14b3368db7c4771dbd9dd5e56eeade3484050df2f98d145cfcf8420cc27ad8aa3392dcf8b444c3d5979998bf915a5e92fd'
